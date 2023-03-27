537a06e9bf77f0df13877b9508e313309c358ca4
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2023-03-27 14:26:04 +0700

    add new block for wti61 extension

---
+++ definition.js
@@ -0,0 +1,265 @@
+const ColorBlock = "#2ECC71";
+Blockly.Blocks['yolobit_motion_start'] = {
+    init: function() {
+      this.jsonInit(
+        {
+          type: "yolobit_sound_start",
+          message0: "khởi tạo cảm biến góc chân TX %1 RX %2",
+          previousStatement: null,
+          nextStatement: null,
+          args0: [
+            {
+              type: "field_dropdown",
+              name: "TX",
+              "options": [
+                [
+                  "P3",
+                  "pin3"
+                ],
+                [
+                  "P0",
+                  "pin0"
+                ],
+                [
+                  "P1",
+                  "pin1"
+                ],
+                [
+                  "P2",
+                  "pin2"
+                ],
+                [
+                  "P4",
+                  "pin4"
+                ],
+                [
+                  "P5",
+                  "pin5"
+                ],
+                [
+                  "P6",
+                  "pin6"
+                ],
+                [
+                  "P7",
+                  "pin7"
+                ],
+                [
+                  "P8",
+                  "pin8"
+                ],
+                [
+                  "P9",
+                  "pin9"
+                ],
+                [
+                  "P10",
+                  "pin10"
+                ],
+                [
+                  "P11",
+                  "pin11"
+                ],
+                [
+                  "P12",
+                  "pin12"
+                ],
+                [
+                  "P13",
+                  "pin13"
+                ],
+                [
+                  "P14",
+                  "pin14"
+                ],
+                [
+                  "P15",
+                  "pin15"
+                ],
+                [
+                  "P16",
+                  "pin16"
+                ],
+                [
+                  "P19",
+                  "pin19"
+                ],
+                [
+                  "P20",
+                  "pin20"
+                ]
+              ]
+            },
+            {
+              "type": "field_dropdown",
+              "name": "RX",
+              "options": [
+                [
+                  "P6",
+                  "pin6"
+                ],
+                [
+                  "P0",
+                  "pin0"
+                ],
+                [
+                  "P1",
+                  "pin1"
+                ],
+                [
+                  "P2",
+                  "pin2"
+                ],
+                [
+                  "P3",
+                  "pin3"
+                ],
+                [
+                  "P4",
+                  "pin4"
+                ],
+                [
+                  "P5",
+                  "pin5"
+                ],              
+                [
+                  "P7",
+                  "pin7"
+                ],
+                [
+                  "P8",
+                  "pin8"
+                ],
+                [
+                  "P9",
+                  "pin9"
+                ],
+                [
+                  "P10",
+                  "pin10"
+                ],
+                [
+                  "P11",
+                  "pin11"
+                ],              
+                [
+                  "P12",
+                  "pin12"
+                ],
+                [
+                  "P13",
+                  "pin13"
+                ],
+                [
+                  "P14",
+                  "pin14"
+                ],
+                [
+                  "P15",
+                  "pin15"
+                ],
+                [
+                  "P16",
+                  "pin16"
+                ],
+                [
+                  "P19",
+                  "pin19"
+                ],
+                [
+                  "P20",
+                  "pin20"
+                ]
+              ]
+            }
+          ],
+          colour: ColorBlock,
+          tooltip: "",
+          helpUrl: ""
+        }
+      );
+    }
+  };
+  Blockly.Python['yolobit_motion_start'] = function(block) {
+    // TODO: Assemble Python into code variable.
+    var tx = block.getFieldValue('TX');
+    var rx = block.getFieldValue('RX');
+    Blockly.Python.definitions_['import_yolobit'] = 'from yolobit import *';
+    Blockly.Python.definitions_['import_motion_wti61'] = 'from yolobit_wti61 import *';
+    Blockly.Python.definitions_['create_motion'] = 'motion = machine.UART(2, baudrate=9600, rx=' + rx + '.pin, tx=' + tx + '.pin)';
+    var code = '';
+    return code;
+};
+
+Blockly.Blocks['yolobit_motion_reset'] = {
+    init: function() {
+      this.jsonInit(
+        {
+            "type": "yolobit_motion_reset",
+            "message0": "đặt lại giá trị góc",
+            "previousStatement": null,
+            "nextStatement": null,
+            "colour": ColorBlock,
+            "tooltip": "",
+            "helpUrl": ""
+        }
+      );
+    }
+};
+Blockly.Python['yolobit_motion_reset'] = function(block) {
+    // TODO: Assemble Python into code variable.
+    var code = 'motion.reset_angle()\n';
+    return code;
+  };
+  Blockly.Blocks['yolobit_get_angle'] = {
+    init: function() {
+      this.jsonInit(
+        {
+            "type": "yolobit_get_angle",
+            "message0": "giá trị góc",
+            "output": null,
+            "colour": ColorBlock,
+            "tooltip": "",
+            "helpUrl": ""
+        }
+      );
+    }
+};
+
+Blockly.Python['yolobit_get_angle'] = function(block) {
+    // TODO: Assemble Python into code variable.
+    var code = 'motion.get_angle()';
+    // TODO: Change ORDER_NONE to the correct strength.
+    return [code, Blockly.Python.ORDER_NONE];
+};
+
+Blockly.Blocks['yolobit_wait_angle'] = {
+    init: function() {
+      this.jsonInit(
+        {
+            "type": "yolobit_wait_angle",
+            "message0": "chờ đến góc %1",
+            "args0": [
+              {
+                "type": "field_number",
+                "name": "wait_angle",
+                "value": 0,
+                "min": -180,
+                "max": 180
+              }
+            ],
+            "output": null,
+            "colour": ColorBlock,
+            "tooltip": "",
+            "helpUrl": ""
+        }
+      );
+    }
+};
+Blockly.Python['yolobit_wait_angle'] = function(block) {
+    var wait_angle = block.getFieldValue('wait_angle');
+    // TODO: Assemble Python into code variable.
+    var code = 'motion.wait_angle('+wait_angle+')';
+    // TODO: Change ORDER_NONE to the correct strength.
+    return [code, Blockly.Python.ORDER_NONE];
+  };
---
+++ yolobit_wti61.py
@@ -0,0 +1,104 @@
+import time
+import math
+from machine import UART, Pin
+from micropython import const
+
+class Motion:
+    def __init__(self, tx_pin, rx_pin):
+        self.tx_pin = tx_pin
+        self.rx_pin = rx_pin
+        self.uart = UART(2, bits=8, parity=None, stop=1, baudrate=115200, rx=self.rx_pin, tx=self.tx_pin)
+        self._buffer = bytearray(11)
+        self.flush_rate = 30
+
+    def reset_angle(self):
+        self.uart.write(b'\xFF\xAA\x52')
+        self.uart.write(b'\xFF\xAA\x67')
+        time_start = time.ticks_ms()
+        time.sleep(0.1)
+        while self.get_angle() > 2:
+            time.sleep_ms(50)
+            if time.ticks_ms() - time_start > 1000:
+                # after 2s then timeout, reset UART connection
+                print('Uart connection to angle sensor error')
+                self.uart.deinit()
+                self.uart = UART(2, bits=8, parity=None, stop=1, baudrate=115200, rx=self.rx_pin, tx=self.tx_pin)
+                self.uart.write(b'\xFF\xAA\x52')
+                self.uart.write(b'\xFF\xAA\x67')
+                self.flush()
+                return
+
+
+    def flush(self):
+        for i in range(self.flush_rate):
+            if self.uart.read() == None:
+                break
+
+    '''
+        both_sign = True: Angle return will from -180 to 180
+        both_sign = False: Angle return will be 0-360
+    '''
+    def get_angle(self, both_sign=True):
+        #self.flush()
+        time_start = time.ticks_ms()
+        counter = 0
+        while True:
+            if self.uart.any():
+                c = self.uart.read(1)
+                if c is None:
+                    continue
+                else:
+                    self._buffer[counter] = c[0]
+                    if counter == 0 and c[0] != 0x55:
+                        continue
+                    counter += 1
+                    if counter == 11:
+                        # process data
+                        if self._buffer[1] == 0x53:
+                            yL = self._buffer[6]
+                            yH = self._buffer[7]
+                            angle = ((yH<<8)|yL)/32768*180
+                            if both_sign:
+                                if angle > 180:
+                                    return (angle - 360)
+                                else:
+                                    return angle
+                            else:
+                                return angle
+                        # reset index
+                        counter = 0
+            if time.ticks_ms() - time_start > 200:
+                print('Read angle timeout - Return 0')
+                return 0
+
+    def wait_angle(self, wait_angle):
+        start_angle = self.get_angle(False)
+        time_start = time.ticks_ms()
+        
+        while True:
+            current_angle = self.get_angle(False)
+            distance = current_angle - start_angle
+            print(current_angle, distance)
+            if wait_angle > 0:
+                if distance > wait_angle:
+                    return
+            else:
+                if distance < wait_angle:
+                    return
+                '''
+                if angle < 1 or angle > 355:
+                    #time.sleep_ms(10)
+                    continue
+                else:
+                    angle = 360-angle
+                    if angle > abs(wait_angle):
+                        return
+                    else:
+                        #time.sleep_ms(10)
+                        continue
+                '''
+            if time.ticks_ms() - time_start > 2500:
+                print('Wait angle timeout')
+                return
+
+motion = Motion()
